97ffd5c1e175229f99b5511ece7ec7c7077298ff
srinivas <srinivasaraochinta@yahoo.com>
2020-09-18 00:51:45 +0530

    first

---
+++ src/main/java/com/wellsfargo/goeurekaserver/GoeurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.goeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoeurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoeurekaserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wellsfargo/goeurekaserver/GoeurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.goeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoeurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
